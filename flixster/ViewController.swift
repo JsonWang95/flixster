de958bf17b5f0c66d3c9b4652bb49bb154365c9e
yuw545@ucsd.edu <yuw545@ucsd.edu>
2020-01-23 17:38:59 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Jason on 2020/1/23.
+//  Copyright © 2020 yuw545@ucsd.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
