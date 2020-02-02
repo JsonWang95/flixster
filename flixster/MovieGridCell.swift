eb9989ac488642a574176aab143912eb5c04c873
yuw545@ucsd.edu <yuw545@ucsd.edu>
2020-02-02 13:46:11 -0800

    Collection View Setup

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jason on 2020/2/1.
+//  Copyright © 2020 yuw545@ucsd.edu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
